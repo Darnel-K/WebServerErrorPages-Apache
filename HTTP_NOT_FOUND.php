e448cdd5ac619024d96a5bb5e7b6785c32e93463
Darnel-K <darnel.kumar91@gmail.com>
2017-12-14 16:53:01 +0000

    -

--- HTTP_NOT_FOUND.php
+++ HTTP_NOT_FOUND.php
@@ -9,16 +9,12 @@
 </head>
 <body>
     <div class="swing">
-        <h1>You Shouldn't Be Here!</h1>
-            <?php 
-                if ($_SERVER['HTTP_HOST'] == gethostbyname($_SERVER['HTTP_HOST'])) {
-                    echo "<h2 id='Error'>Direct IP access is forbidden</h2>";
-                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access this web server directly using it's IP address.</p></section>";
-                } else {
-                    echo "<h2 id='Error'>Host '" . $_SERVER['HTTP_HOST'] . "' does not exist on this server</h2>";
-                    echo "<section><h3>Just in case you were wondering...</h3><p>You are seeing this error message because you tried to access a host that does not exist on this server.</p></section>";
-                }
-            ?>
+        <h1>Object not found!</h1>
+        <h2 id='Error'>Error 404</h2>
+        <section>
+            <h3>Just in case you were wondering...</h3>
+            <p>The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.</p>
+        </section>
         <p id="info"><?php echo date("d/m/Y h:i:s A T"); ?> &bull; <?php echo date("e"); ?> &bull; Your IP <?php echo $_SERVER['REMOTE_ADDR']; ?> &bull; <a href="mailto:<?php echo $_SERVER['SERVER_ADMIN']; ?>">Contact Server Admin</a></p>
     </div>
     <footer><?php echo $_SERVER['SERVER_SIGNATURE']; ?></footer>
