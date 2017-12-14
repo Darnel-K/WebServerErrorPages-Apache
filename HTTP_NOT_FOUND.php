6d7ec95f69b336f909086b1eda3ec9e3d532135b
Darnel-K <darnel.kumar91@gmail.com>
2017-12-14 17:06:15 +0000

    -

--- HTTP_NOT_FOUND.php
+++ HTTP_NOT_FOUND.php
@@ -8,6 +8,13 @@
     <title>Object not found!</title>
 </head>
 <body>
+    <?php
+        $date = date("d/m/Y h:i:s A T");
+        $region = date("e");
+        $userIP = $_SERVER['REMOTE_ADDR'];
+        $admin = $_SERVER['SERVER_ADMIN'];
+        $signature = $_SERVER['SERVER_SIGNATURE'];
+    ?>
     <div class="swing">
         <h1>Object not found!</h1>
         <h2 id='Error'>Error 404</h2>
@@ -15,8 +22,8 @@
             <h3>Just in case you were wondering...</h3>
             <p>The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.<br>If you think this is a server error, please contact the server admin using the button below.</p>
         </section>
-        <p id="info"><?php echo date("d/m/Y h:i:s A T"); ?> &bull; <?php echo date("e"); ?> &bull; Your IP <?php echo $_SERVER['REMOTE_ADDR']; ?> &bull; <a href="mailto:<?php echo $_SERVER['SERVER_ADMIN']; ?>">Contact Server Admin</a></p>
+        <p id="info"><?php echo $date; ?> &bull; <?php echo $region; ?> &bull; Your IP <?php echo $userIP; ?> &bull; <a href="mailto:<?php echo $admin; ?>">Contact Server Admin</a></p>
     </div>
-    <footer><?php echo $_SERVER['SERVER_SIGNATURE']; ?></footer>
+    <footer><?php echo $signature; ?></footer>
 </body>
 </html>
