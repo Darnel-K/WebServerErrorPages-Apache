24c9ddacf1afcced87b8985f578f82177cb72a0e
Darnel-K <darnel.kumar91@gmail.com>
2017-12-15 14:35:08 +0000

    -

--- HTTP_ERROR.php
+++ HTTP_ERROR.php
@@ -1,18 +1,17 @@
 <?php
-http_response_code(418);
     $date = date("d/m/Y h:i:s A T");
     $region = date("e");
     $userIP = $_SERVER['REMOTE_ADDR'];
     $admin = $_SERVER['SERVER_ADMIN'];
     $signature = $_SERVER['SERVER_SIGNATURE'];
-    // $ResponseCode = $_SERVER['REDIRECT_STATUS'];
-    // require 'Codes.php';
-    // try {
-    //     $ErrorTitle = $ErrorCodes[$ResponseCode][0];
-    //     $ErrorMessage = $ErrorCodes[$ResponseCode][1];
-    // } catch (Exception $e) {
-
-    // }
+    $ResponseCode = $_SERVER['REDIRECT_STATUS'] || http_response_code();
+    require 'Codes.php';
+    try {
+        $ErrorTitle = $ErrorCodes[$ResponseCode][0];
+        $ErrorMessage = $ErrorCodes[$ResponseCode][1];
+    } catch (Exception $e) {
+        
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,7 +26,6 @@ http_response_code(418);
     <div class="swing">
         <h1><?php echo $ErrorTitle; ?></h1>
         <h2 id='Error'>Error <?php echo $ResponseCode; ?></h2>
-        <p><?php echo $_SERVER['REDIRECT_STATUS']; ?>, <?php echo http_response_code(); ?></p>
         <section>
             <h3>Just in case you were wondering...</h3>
             <p><?php echo $ErrorMessage; ?><br><br>If you think this is a server error, please contact the server admin using the link below.</p>
