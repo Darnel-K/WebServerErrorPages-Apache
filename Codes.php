8221f741639d6627c7f94af0c0802aa04103d8ae
Darnel-K <darnel.kumar91@gmail.com>
2017-12-15 13:27:49 +0000

    -

--- Codes.php
+++ Codes.php
@@ -1,7 +1,7 @@
 <?php
     $ErrorCodes = array(
         400 => array('Bad request!', 'Your browser (or proxy) sent a request that this server could not understand.'),
-        401 => array('Authentication required!', 'This server could not verify that you are authorized to access the URL "' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '". You either supplied the wrong credentials (e.g., bad password), or your browser doesn\'t understand how to supply the credentials required. In case you are allowed to request the document, please check your user-id and password and try again.'),
+        401 => array('Authentication required!', 'This server could not verify that you are authorized to access the URL "' . $_SERVER['REDIRECT_SERVER_NAME'] . $_SERVER['REDIRECT_URL'] . '". You either supplied the wrong credentials (e.g., bad password), or your browser doesn\'t understand how to supply the credentials required. In case you are allowed to request the document, please check your user-id and password and try again.'),
         403 => array(),
         404 => array('Object not found!', 'The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.'),
         405 => array(),
