7910290e27eeecb5e0bb34b004ff7b9c08b30cbd
Darnel-K <darnel.kumar91@gmail.com>
2017-12-15 14:30:24 +0000

    -

--- HTTP_ERROR.php
+++ HTTP_ERROR.php
@@ -4,10 +4,14 @@
     $userIP = $_SERVER['REMOTE_ADDR'];
     $admin = $_SERVER['SERVER_ADMIN'];
     $signature = $_SERVER['SERVER_SIGNATURE'];
-    $ResponseCode = $_SERVER['REDIRECT_STATUS'];
-    require 'Codes.php';
-    $ErrorTitle = $ErrorCodes[$ResponseCode][0];
-    $ErrorMessage = $ErrorCodes[$ResponseCode][1];
+    // $ResponseCode = $_SERVER['REDIRECT_STATUS'];
+    // require 'Codes.php';
+    // try {
+    //     $ErrorTitle = $ErrorCodes[$ResponseCode][0];
+    //     $ErrorMessage = $ErrorCodes[$ResponseCode][1];
+    // } catch (Exception $e) {
+
+    // }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,6 +26,7 @@
     <div class="swing">
         <h1><?php echo $ErrorTitle; ?></h1>
         <h2 id='Error'>Error <?php echo $ResponseCode; ?></h2>
+        <p><?php echo $_SERVER['REDIRECT_STATUS']; ?>, <?php echo http_response_code(); ?></p>
         <section>
             <h3>Just in case you were wondering...</h3>
             <p><?php echo $ErrorMessage; ?><br><br>If you think this is a server error, please contact the server admin using the link below.</p>
