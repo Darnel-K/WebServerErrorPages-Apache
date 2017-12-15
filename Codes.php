970094a8474f7062d27ff001a9ec7c90476b0e6f
Darnel-K <darnel.kumar91@gmail.com>
2017-12-15 13:15:38 +0000

    -

--- Codes.php
+++ Codes.php
@@ -1,5 +1,5 @@
 <?php
     $ErrorCodes = array(
-        404 => array('Object not found!', 'Test'),
+        404 => array('Object not found!', 'The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.'),
     );
 ?>
